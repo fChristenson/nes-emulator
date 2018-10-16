38ffb72ad7641c09a256e199852e770ba3bb2e6e
Fredrik Christenson <fredrik@minatjanster.se>
2018-10-16 14:14:09 +0200

    cpu

--- src/lib/cpu.rs
+++ src/lib/cpu.rs
@@ -1,8 +1,29 @@
+pub struct P {
+  pub value: u8
+}
+
+impl P {
+  fn get_bit(&self, n: u8) -> u8 {
+    let shift = self.value >> n; //TODO: how to impl >>>
+    shift & 0x1
+  }
+
+  fn toggle_bit(&mut self, n: u8) {
+    self.value = self.value ^ (1 << n)
+  }
+
+  fn set_bit(&mut self, n: u8, v: u8) {
+    if self.get_bit(n) != v {
+      self.toggle_bit(n)
+    }
+  }
+}
+
 pub struct Cpu {
   pub x: u8,
   pub y: u8,
   pub a: u8,
-  pub p: u8,
+  pub p: P,
   pub program_counter: u8,
   pub stack_pointer: u8,
 }
